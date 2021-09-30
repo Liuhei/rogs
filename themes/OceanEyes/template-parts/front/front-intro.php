dace9749f7594b65de6e407a02da6cb8140f5dfb
Liuhei <liuhei.yamada@gmail.com>
2021-09-30 18:24:12 +0900

    favicon/intro section fixed

--- themes/OceanEyes/template-parts/front/front-intro.php
+++ themes/OceanEyes/template-parts/front/front-intro.php
@@ -1,8 +1,8 @@
 <div class="fp-intro-left">
 	<div class="fp-intro-inner">
 		<p class="fp-intro-title">Sup, it's Ryu!</p>
-		<p class="fp-intro-content">初めまして。Ryuと申します。某プログラミングスクールで講師をした後、5年間フロントエンドで開発をしています。あ、英語も話せますよ。</p>
-		<a href="#" class="fp-intro-button">詳しい経歴を見てみる</a>
+		<p class="fp-intro-content">初めまして、Ryuです。フロントエンジニア歴3年です。某プログラミングスクールで講師をした後、投資関連会社のWebチームに従事、その後はアフィリエイター（仕事系・語学系）。メインのサイトを130万円/その他数万円で売却し、現在(2021年)は職探し中。</p>
+		<a href="<?php echo esc_url(site_url('/contact-form')); ?>" class="fp-intro-button">連絡してみる</a>
 	</div>
 	<div class="ball"></div>
 	<div class="ball2"></div>
